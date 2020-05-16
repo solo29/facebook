e32178bf2387866daf5542573abc0ba12743d98b
Solo <29plazma@gmail.com>
2020-05-16 22:45:27 +0400

    sidebar done

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
-        <Nav></Nav>
+  <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+    <Nav></Nav>
+    <div class="overflow-y-hidden flex-1 flex">
+      <SideBar></SideBar>
+      <div class="overflow-x-hidden w-2/3">
         <router-view></router-view>
+        <li v-for="i in 30" :key="i">qweqwe</li>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import Nav from "./Nav";
+import SideBar from "./SideBar";
 export default {
-    name: "App",
-    components: {
-        Nav
-    },
-    mounted() {
-        console.log("Component mounted.");
-    }
+  name: "App",
+  components: {
+    Nav,
+    SideBar
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
 };
 </script>
---
+++ resources/js/components/SideBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-1/3 bg-white p-4 border-r border-gray-400">
+    <h2 class="font-bold tracking-tight text-2xl">Home</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "SideBar"
+};
+</script>

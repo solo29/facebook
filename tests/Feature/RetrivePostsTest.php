a02bff3d0f632af5937cafe3dcb906730c9aac77
Solo <29plazma@gmail.com>
2020-05-20 00:25:41 +0400

    retrive freinds posts

--- tests/Feature/RetrivePostsTest.php
+++ tests/Feature/RetrivePostsTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Friend;
 use App\Post;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -65,6 +67,49 @@ class RetrivePostsTest extends TestCase
 
         $response->assertExactJson([
             'data' => [],
+            'links' => [
+                'self' => url('/posts')
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_retrive_friends_posts()
+    {
+        $this->withoutExceptionHandling();
+        $user = $this->signIn();
+        $anotherUser =  factory(User::class)->create();
+        $posts = factory(Post::class, 2)->create(['user_id' => $anotherUser->id]);
+
+        Friend::create(['user_id' => $user->id, 'friend_id' => $anotherUser->id, 'confirmed_at' => now(), 'status' => 1]);
+
+        $response = $this->get('/api/posts')->assertStatus(200);
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'data' => [
+                        'type' => 'posts',
+                        'post_id' => $posts->last()->id,
+                        'attributes' => [
+                            'body' => $posts->last()->body,
+                            'image' => $posts->last()->image,
+                            'posted_at' => $posts->last()->created_at->diffForHumans()
+                        ]
+                    ]
+                ],
+                [
+                    'data' => [
+                        'type' => 'posts',
+                        'post_id' => $posts->first()->id,
+                        'attributes' => [
+                            'body' => $posts->first()->body,
+                            'image' => $posts->first()->image,
+                            'posted_at' => $posts->first()->created_at->diffForHumans()
+                        ]
+                    ]
+                ]
+            ],
             'links' => [
                 'self' => url('/posts')
             ]
